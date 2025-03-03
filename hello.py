57acacdb6753b4446beac135eb9a7dcb86fd30ae
joaolack <jaoglack@proton.me>
2025-03-03 16:58:58 -0300

    add multilanguages Hello World

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
+import os
 
-print("Hello World") # This is a comment
+current_lang = os.getenv("LANG", "en_US")[:5]
+msg = ("Hello World")
+
+if current_lang == "pt_BR":
+    msg = ("Olá Mundo")
+elif current_lang == "es_ES":
+    msg = ("Hola Mundo")
+elif current_lang == "fr_FR":
+    msg = ("Bonjour le monde")    
+
+print(msg) 
